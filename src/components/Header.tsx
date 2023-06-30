f737ed8d0a0fecdd9cacaad45abf07b8bd6e517a
codersuresh <sd807141@gmail.com>
2023-06-30 12:54:46 +0545

    fix: hide scrollbar on other browser also

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 const Header = () => {
   return (
       <header>
-          <h1 className="text-4xl font-bold text-center mt-10 mb-5">Img<span className='text-orange-600/'>hub</span></h1>
+          <h1 className="text-4xl font-bold text-center mt-10 mb-5">Img<span className='text-orange-600'>hub</span></h1>
           <nav className='overflow-auto hide_scrollbar'>
               <ul className="flex justify-center gap-5">
                   <li><Link href="#" className='hover:text-orange-600 transition-colors font-medium'>Nature</Link></li>
