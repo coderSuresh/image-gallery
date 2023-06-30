f1680fd19e664cad264290a5c599b9375e9470ca
codersuresh <sd807141@gmail.com>
2023-06-30 12:26:14 +0545

    feat: make nav scrollable on mobile

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,8 +3,8 @@ import Link from 'next/link'
 const Header = () => {
   return (
       <header>
-          <h1 className="text-4xl font-bold text-center mt-10 mb-5">Img<span className='text-orange-600'>hub</span></h1>
-          <nav>
+          <h1 className="text-4xl font-bold text-center mt-10 mb-5">Img<span className='text-orange-600/'>hub</span></h1>
+          <nav className='overflow-auto hide_scrollbar'>
               <ul className="flex justify-center gap-5">
                   <li><Link href="#" className='hover:text-orange-600 transition-colors font-medium'>Nature</Link></li>
                   <li><Link href="#" className='hover:text-orange-600 transition-colors font-medium'>Technology</Link></li>
