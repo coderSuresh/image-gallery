09c37d6ca4ec6a61f4be6d6132c88fd4b1a6b497
codersuresh <sd807141@gmail.com>
2023-06-30 11:35:17 +0545

    fix: show gallery

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
-// import FetchImages from '@/components/FetchImages'
+import FetchImages from '@/components/FetchImages'
 
-// const photoElem = FetchImages()
+const photoElem = FetchImages()
 
 const Home = () => {
   return (
     <main>
-      {/* <div className="gallery flex gap-5 justify-center mt-10 flex-wrap">{photoElem}</div> */}
+      <div className="gallery flex gap-5 justify-center mt-10 flex-wrap">{photoElem}</div>
     </main>
   )
 }
